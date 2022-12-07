e18b11c5745ab80b2692787fd0899178b6eaf856
Yaacov Akiba Slama <ya@slamail.org>
2022-12-07 16:45:28 +0200

    Add HNL and NGN

--- main.go
+++ main.go
@@ -95,7 +95,8 @@ func getYadioRates() (map[string]string, error) {
 		"CNY", "JPY", "CAD", "AUD", "SGD", "CHF", "SEK", "KRW", "INR", "NOK",
 		"TTD", "PYG", "TRY", "GTQ", "CRC", "ILS", "PAB", "VND", "AED", "HKD",
 		"IDR", "DKK", "BOB", "NZD", "PHP", "CZK", "PLN", "PKR", "ZAR", "NAD",
-		"RON", "ANG", "JMD", "HUF", "MYR", "XOF", "THB", "TWD"} {
+		"RON", "ANG", "JMD", "HUF", "MYR", "XOF", "THB", "TWD", "HNL", "NGN",
+	} {
 		usdCurrency := gjson.Get(json, "USD."+currency).Float()
 		cur, _ := (new(big.Float).Mul(bigUsd, big.NewFloat(usdCurrency))).Float64()
 		rates[currency] = fmt.Sprintf("%f", cur)
